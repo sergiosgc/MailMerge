4b132a13f269ddb6c91420065938aad1510293c8
Sérgio Carvalho <sergiosgc@gmail.com>
2022-05-18 00:58:18 +0100

    Add project files.

---
+++ MergeJob.cs
@@ -0,0 +1,74 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailMerge
+{
+    class MergeJob
+    {
+        private string attachment;
+        private string recipient;
+        private List<string> carbonCopy = new List<string>();
+
+        public MergeJob(string[] columns)
+        {
+            attachment = columns[0];
+            recipient = columns[1];
+            for (int i = 2; i < columns.Count() - 1; i++) carbonCopy.Add(columns[i]);
+        }
+
+        public override string ToString()
+        {
+            List<string> parts = new List<string>();
+            parts.Add(attachment);
+            parts.Add(recipient);
+            parts.Concat(carbonCopy);
+            StringBuilder result = new StringBuilder();
+            string separator = "";
+            foreach (string part in parts)
+            {
+                result.Append(separator);
+                separator = ",";
+                result.Append('"' + part.Replace("\"", "\"\"") + '"');
+            }
+            return result.ToString();
+        }
+
+        internal void execute(string emailTemplate, string emailServer, string emailUsername, string emailPassword)
+        {
+            var message = MimeMessage.Load(File.OpenRead(emailTemplate));
+            message.To.Clear();
+            message.Cc.Clear();
+            message.To.Add(new MailboxAddress("", recipient));
+            message.Date = DateTimeOffset.Now;
+            message.MessageId = MimeUtils.GenerateMessageId();
+            foreach (string cc in this.carbonCopy) message.Cc.Add(new MailboxAddress("", cc));
+            var newBody = new Multipart("mixed");
+            newBody.Add(message.Body); 
+            newBody.Add(new MimePart("application", "pdf")
+            {
+                Content = new MimeContent(File.OpenRead(this.attachment),
+                                          ContentEncoding.Default),
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                ContentTransferEncoding = ContentEncoding.Base64,
+                FileName = Path.GetFileName(this.attachment)
+            });
+            
+            message.Body = newBody;
+            using (var client = new SmtpClient())
+            {
+                client.Connect(emailServer, 465, SecureSocketOptions.SslOnConnect);
+                client.Authenticate(emailUsername, emailPassword);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MailMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shouldShowHelp = false;
+            string emailTemplate = "";
+            string emailServer = "";
+            string emailUsername = "";
+            string emailPassword = "";
+            string csvFile = "";
+            
+            var options = new OptionSet {
+                { "t|template=", "Email template file.", (string t) => emailTemplate = t },
+                { "server=", "SMTP server", (string s) => emailServer = s },
+                { "username=", "SMTP username", (string s) => emailUsername = s },
+                { "password=", "SMTP password", (string s) => emailPassword = s },
+                { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+            };
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+                if (extra.Count > 0) csvFile = extra[0];
+            }
+            catch (OptionException e)
+            {
+                ShowHelp(options, e.Message);
+            }
+            if (shouldShowHelp)
+            {
+                ShowHelp(options);
+                return;
+            }
+            if (csvFile == "") { ShowHelp(options, "Missing csvFile"); return; }
+            if (emailTemplate == "") { ShowHelp(options, "Missing template"); return; }
+            if (emailServer == "") { ShowHelp(options, "Missing server"); return; }
+            if (emailUsername == "") { ShowHelp(options, "Missing username"); return; }
+            if (emailPassword == "") { ShowHelp(options, "Missing password"); return; }
+            using (TextFieldParser parser = new TextFieldParser(csvFile))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    MergeJob job = new MergeJob(parser.ReadFields());
+                    try
+                    {
+                        job.execute(emailTemplate, emailServer, emailUsername, emailPassword);
+                    } catch (System.Exception e)
+                    {
+                        Console.WriteLine(job.ToString() + ",\"" + e.Message + "\"");
+                    }
+
+                }
+            }
+        }
+        private static void ShowHelp(OptionSet p, String error = "")
+        {
+            if (error != "")
+            {
+                Console.WriteLine("Error: " + error);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Usage: mailmerge.exe [OPTIONS]+ csvFile");
+            Console.WriteLine("Send one email per row of csvFile. The CSV file should");
+            Console.WriteLine("contain one column with a filename to be attached, then");
+            Console.WriteLine("one column with the primary recipient email (To field)");
+            Console.WriteLine("followed by a variable number of columns containing");
+            Console.WriteLine("secondary recipients (CC). The last column is ignored");
+            Console.WriteLine();
+            Console.WriteLine("The program outputs a CSV file with the same format, with");
+            Console.WriteLine("one row per failed email merge. The last column contains the");
+            Console.WriteLine("error message. This file may be fed again into mailmerge.exe");
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
